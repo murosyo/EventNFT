2bd8c1b1fef04c4e2e92bd4111742246db46b43a
murosyo <murozuka.shota@mikilab.doshisha.ac.jp>
2023-08-14 14:21:26 +0900

    要約した文章の翻訳機能を実装

--- api/imageGeneration/deepl.py
+++ api/imageGeneration/deepl.py
@@ -2,7 +2,7 @@ import requests
 
 API_KEY = '04a0b924-7924-fb14-60cf-5c4a577ac4d3:fx' # 自身の API キーを指定
 
-def translate(text):
+def translate_en_to_ja(text):
     text = text
     source_lang = 'EN'
     target_lang = 'JA'
@@ -19,4 +19,25 @@ def translate(text):
     request = requests.post("https://api-free.deepl.com/v2/translate", data=params) # URIは有償版, 無償版で異なるため要注意
     result = request.json()
 
-    print(result)
+    # print(result)
+    return result['translations'][0]["text"]
+
+def translate_ja_to_en(text):
+    text = text
+    source_lang = 'JA'
+    target_lang = 'EN'
+
+    # パラメータの指定
+    params = {
+                'auth_key' : API_KEY,
+                'text' : text,
+                'source_lang' : source_lang, # 翻訳対象の言語
+                "target_lang": target_lang  # 翻訳後の言語
+            }
+
+    # リクエストを投げる
+    request = requests.post("https://api-free.deepl.com/v2/translate", data=params) # URIは有償版, 無償版で異なるため要注意
+    result = request.json()
+
+    # print(result)
+    return result['translations'][0]["text"]
--- api/imageGeneration/main.py
+++ api/imageGeneration/main.py
@@ -1,14 +1,38 @@
+import re
 from summerize import summerize
-from deepl import translate
-from Rake import _preprocess
+from deepl import translate_en_to_ja
+from deepl import translate_ja_to_en
+# from Rake import _preprocess
+from fastapi import FastAPI
+app = FastAPI()
 
-def main():
-    f = open('test.txt', 'r')
+@app.get("/")
+async def main():
+    # f = open('test_en.txt', 'r')
+    f = open('test_ja.txt', 'r')
     all_text = f.read()
     f.close()
+    # summerize APIが日本語の要約できないみたいなので、日本語文なら一旦英語に翻訳
+    IsJapanese = contains_japanese(all_text)
+    if IsJapanese:
+        all_text = translate_ja_to_en(all_text)
+    else:
+        all_text = all_text
+    
+    # 文章の要約
     summary_text = summerize(all_text)
-    # print(summary_text)
 
+    # 英語から日本語に翻訳
+    trans_text = translate_en_to_ja(summary_text)
+    # 改行コード（\n）の除去
+    trans_text = re.sub(r'\n', '', trans_text)
+    # return summary_text
+    return trans_text
+
+def contains_japanese(text):
+    # 正規表現で日本語の文字を検索
+    pattern = re.compile(r'[ぁ-んァ-ン一-龥]')
+    return bool(pattern.search(text))
 
 if __name__ == "__main__":
     main()
